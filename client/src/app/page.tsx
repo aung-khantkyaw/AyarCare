106da0be9e0a2d633dd7cf7d699b896d19f8481f
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2025-04-14 02:08:42 +0630

    added: Client side WebApp Using NextJS

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-screen">
+       <h1 className="text-7xl font-bold">
+           <span className="text-green-500">Ayar</span><span className="text-blue-500">Care</span>
+       </h1>
+    </div>
+  );
+}
